a04172e15b03074e590ac94bfcc61342e368402b
migh1 <lucasmigli1@gmail.com>
2017-10-30 01:55:14 -0200

    meu deus

--- app.js
+++ app.js
@@ -15,17 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors());
-
-/*app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});*/
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -42,7 +35,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-app.use(function(request, response) {
+/*app.use(function(request, response) {
 	if (request.method == 'OPTIONS') {
 		response.setHeader('Access-Control-Allow-Origin', '*');
 		response.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
@@ -53,7 +46,7 @@ app.use(function(request, response) {
 		response.setHeader('Access-Control-Allow-Origin', '*');
 		response.end('foobar');
 	}
-});
+});*/
 
 // error handler
 app.use(function(err, req, res, next) {
