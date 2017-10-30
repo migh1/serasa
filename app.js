c710bf1a29f2a8d0b34466fe492d8e62884e1793
migh1 <lucasmigli1@gmail.com>
2017-10-30 01:54:05 -0200

    meu deus

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ app.set('view engine', 'ejs');
 
 app.use(cors());
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
-});
+});*/
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
